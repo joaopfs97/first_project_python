e64a2e72b0066456d92a4f96eebdfd8e6832397c
joaopfs97 <joaopfs201@gmail.com>
2025-08-14 15:10:45 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+
+def exibir_nome_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Programa finalizado')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    match(opcao_escolhida):
+        case 1:
+            print('Cadastrar Restaurante')
+        case 2:
+            print('Listar restaurante')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
